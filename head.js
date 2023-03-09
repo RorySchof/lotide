626f5948ae83b3e5694557699bf3e58824c66238
RorySchof <roryschofield@gmail.com>
2023-03-09 14:30:58 -0500

    lotide 2 commit

---
+++ head.js
@@ -0,0 +1,34 @@
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+    return true;
+
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+    return false;
+  }
+   
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+
+
+
+
+
+//module.exports = head;
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
