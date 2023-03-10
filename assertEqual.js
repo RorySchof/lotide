9d4b93313751cc809a8fb2840e15fb18b1bd1c20
RorySchof <roryschofield@gmail.com>
2023-03-09 20:39:01 -0500

    Fixing Lotide

--- assertEqual.js
+++ assertEqual.js
@@ -17,5 +17,4 @@ const assertEqual = function(actual, expected) {
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
-# lotide
-# Test-Repo
+
---
+++ eqArrays.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = require('./assertEqual');
+
+  const eqArrays = function(arr1,arr2){
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+
+    for (let i =0; 1,arr1.length; i++) {
+      return false;
+    }
+  
+  return true;
+   
+};
+
+// TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+
