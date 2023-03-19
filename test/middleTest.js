a0e1aaf1f5ade7db04dbd7f051d881d40bbb00b9
RorySchof <roryschofield@gmail.com>
2023-03-19 19:32:55 -0400

    Refactored with Mocha and Chai

---
+++ test/middleTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+//this passed on node and is ready for submission. 
+
---
+++ test/tailTest.js
@@ -0,0 +1,9 @@
+
+const assertEqual = require('../assertEqual');
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
